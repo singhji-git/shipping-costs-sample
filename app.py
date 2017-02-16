1be738d4877073608a90aff32c7b27c460b82d53
saurav kumar <sauarvk048@gmail.com>
2017-02-17 01:19:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,14 +31,15 @@ def webhook():
 
 def makeWebhookResult(req):
     x = req.get("result").get("action")
-    if  x != "current.plan" and  (x != "current.planchange" and x != "bill.enquiry"):
-        return {}
+#     if  x != "current.plan" and  (x != "current.planchange" and x != "bill.enquiry"):
+#         return {}
     
     result = req.get("result")
     parameters = result.get("parameters")
     speech="hey";
     plan = {'9155465072':"Free Roaming", '9572390164':"Free Calling", '919973212':"Free 1GB Data", '9973617212':"30p/min", '91998870950':"Free Videocalling"}
     bill = {'9155465072': "100" , '9572390164': "200" , '919973212': "300" , '9973617212': "350.45" , '91998870950': "345.23" }
+    subscription = {'9155465072': "callerTuneActivated" , '9572390164': "none" , '919973212': "callerTuneActivated" , '9973617212': "none" , '91998870950': "callerTuneActivated" }
     if req.get("result").get("action") == "current.plan":
         number = parameters.get("Phonenumber")
         comp= parameters.get("Company")
@@ -52,6 +53,13 @@ def makeWebhookResult(req):
     elif req.get("result").get("action") == "bill.enquiry":
         number = parameters.get("Phonenumber")
         speech = "The bill for   " + number + " is " + str(bill[number]) + "."
+    elif req.get("result").get("action") == "bill.highcomplain" :
+        number = parameters.get("Phonenumber")
+        subscription1 = subscription[number]
+        if subscription1 == "none" :
+            speech = "you phone no. " + number + " is not subscribed for any featues.."
+        else :
+            speech = "you phone no. " + number + " is  subscribed to " + subscription1 + "."
     else :
         speech = "hey"
         
